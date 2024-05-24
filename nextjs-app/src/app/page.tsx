efcf332cbaef1cee2dde9b1c221b59a5cd8e1f68
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-05-24 17:00:12 +0200

    style: initial styling settings

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-black">Docunder</h1>
-
+      <h1 className="text-h1 text-neutral-2 font-weight-bold">Docunder</h1>
+      <button className="custom-btn"><span>Acesse</span></button>
     </main>
   );
 }
