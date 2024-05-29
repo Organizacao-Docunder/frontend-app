c3228bcf123a450559616419bfa7fbec4e46eecd
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-05-29 15:27:19 +0200

    init: login page

---
+++ nextjs-app/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-h1 text-neutral-2 font-weight-bold">Login</h1>
+    </main>
+  );
+}
