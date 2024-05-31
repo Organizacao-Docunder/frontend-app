05805f0bd488862533cb35955a949e85979c4336
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-05-31 16:35:56 +0200

    feat: links

--- nextjs-app/src/components/Header.tsx
+++ nextjs-app/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HighlightBtn from "./HighlightBtn";
 import logo from '../assets/logo.svg'
+import Link from "next/link";
 
 export default function Header({ }) {
   return (
@@ -9,15 +10,17 @@ export default function Header({ }) {
     >
       <div className="max-w-7xl w-full h-full flex justify-between">
         <div className="flex items-center w-1/5 justify-center">
-          <Image alt="logo" src={logo} className="flex" />
+          <Link href={"/"}>
+            <Image alt="logo" src={logo} className="flex" />
+          </Link>
         </div>
         <div className="flex w-4/5 items-center justify-end gap-12">
-          <h5 className="text-h5 text-neutral-2">Sobre</h5>
-          <h5 className="text-h5 text-neutral-2">Entrar</h5>
+          <Link href={"/about"}><h5 className="text-h5 text-neutral-2">Sobre</h5></Link>
+          <Link href={"/login"}><h5 className="text-h5 text-neutral-2">Entrar</h5></Link>
           <h5 className="text-h5 text-neutral-2">Perguntas frequentes</h5>
-          <HighlightBtn id={"start-free"} content={"Comece grátis"} paddingX={'px-4'}/>
+          <HighlightBtn id={"start-free"} content={"Comece grátis"} customStyle={'px-4'} link={"/signup"}/>
         </div>
       </div>
-    </header>
+    </header >
   );
 }
