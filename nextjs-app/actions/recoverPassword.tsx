961177516c6b60c3adc9fd01fe7ef941a2c506c5
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-06-28 18:20:06 +0200

    feat: recover password

--- nextjs-app/actions/recoverPassword.tsx
+++ nextjs-app/actions/recoverPassword.tsx
@@ -9,29 +9,119 @@ export async function getQuestionsByEmail(email) {
   }
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
+
   try {
     if (email.length === 0) {
-      throw new Error('Digite seu email.', { cause: 401 });
+      throw new Error('Digite seu email.', { cause: 'email' });
     }
     if (email.length < 6 || email.length > 74) {
-      throw new Error('O e-mail deve possuir entre 6 e 74 caracteres.', { cause: 401 });
+      throw new Error('O e-mail deve possuir entre 6 e 74 caracteres.', { cause: 'email' });
     }
     if (!emailRegex.test(email)) {
-      throw new Error('E-mail inválido.', { cause: 401 });
+      throw new Error('E-mail inválido.', { cause: 'email' });
     }
 
     const response = await axios.post('http://localhost:3000/user/recover-password', body);
-    return { status: 200, message: "", questions: response.data};
+    return { status: 200, cause: '', message: "OK", questions: response.data };
   } catch (error) {
     if (axios.isAxiosError(error)) {
       if (error.response) {
-        return { status: 401, message: 'O e-mail não confere', questions: []};
+        return { status: 401, cause: 'email', message: 'O e-mail não confere', questions: [] };
       }
     }
     if (error instanceof Error) {
-      return { status: error.cause, message: [error.message], questions: [] };
+      return { status: 401, cause: error.cause, message: [error.message], questions: [] };
     }
-    return { status: 401, message: 'Bad Request', questions:[] };
+    return { status: 401, cause: 'email', message: 'Bad Request', questions: [] };
+  }
+}
+
+export async function getSecretAnswer(email, questionAndAnswer) {
+
+  const body = {
+    email: email,
+    questionId: Number(questionAndAnswer.questionId),
+    answer: questionAndAnswer.answer
+  }
+
+  try {
+    if (questionAndAnswer.questionId === 0) {
+      throw new Error('Escolha uma pergunta.', { cause: 'question' });
+    }
+    if (questionAndAnswer.answer.length === 0) {
+      throw new Error('Digite sua resposta.', { cause: 'answer' });
+    }
+
+    const response = await axios.post('http://localhost:3000/user/verify-secret-answer', body);
+    return { status: 200, cause: '', message: "OK", questions: response.data };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        return { status: 401, cause: 'answer', message: 'A resposta está incorreta.', questions: [] };
+      }
+    }
+    if (error instanceof Error) {
+      return { status: 401, cause: error.cause, message: error.message, questions: [] };
+    }
+    return { status: 401, cause: 'answer', message: 'Bad Request', questions: [] };
+  }
+}
+
+export async function recoverPassword(email, newPassword) {
+  const has6Char = /^.{6,}$/;
+  const hasDigit = /(?=.*\d)/;
+  const hasSpecialChar = /(?=.*[!@#\$%\^&\*\(\)_\+\-=\[\]\{\};':",\.<>\/\?\\|`~])/;
+  const hasUpperCase = /(?=.*[A-Z])/;
+  const hasLowerCase = /(?=.*[a-z])/;
+  const noSpaceOrNewline = /^[^\s\n\r]+$/;
+  const anyCharsToEnd = /.*/;
+
+  try {
+    if (newPassword.password.length < 6 || newPassword.password.length > 74) {
+      throw new Error('A senha deve possuir entre 6 e 74 caracteres.', { cause: 'password' });
+    }
+    if (!has6Char.test(newPassword.password)) {
+      throw new Error('Adicione pelo menos 6 caracteres.', { cause: 'password' });
+    }
+    if (!hasDigit.test(newPassword.password)) {
+      throw new Error('Adicione pelo menos um dígito.', { cause: 'password' });
+    }
+    if (!hasSpecialChar.test(newPassword.password)) {
+      throw new Error('Adicione pelo menos um caractere especial.', { cause: 'password' });
+    }
+    if (!hasUpperCase.test(newPassword.password)) {
+      throw new Error('Adicione pelo menos uma letra maiúscula.', { cause: 'password' });
+    }
+    if (!hasLowerCase.test(newPassword.password)) {
+      throw new Error('Adicione pelo menos uma letra minúscula.', { cause: 'password' });
+    }
+    if (!noSpaceOrNewline.test(newPassword.password)) {
+      throw new Error('A senha não deve conter espaço ou nova linha.', { cause: 'password' });
+    }
+    if (!anyCharsToEnd.test(newPassword.password)) {
+      throw new Error('A senha é inválida.', { cause: 'password' });
+    }
+
+    //password confirmation
+    if (newPassword.matchPassword.length === 0) {
+      throw new Error('Confirme sua senha.', { cause: 'match password' });
+    }
+    if (newPassword.password !== newPassword.matchPassword) {
+      throw new Error('Confirmação de senha incorreta.', { cause: 'match password' });
+    }
+
+    const body = {
+      email: email,
+      newPassword: newPassword.password
+    }
+
+    await axios.post('http://localhost:3000/user/reset-password', body);
+    return { status: 200, cause: '', message: '', questions: [] };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message)
+      return { status: 401, cause: error.cause, message: error.message, questions: []};
+    }
+    return { status: 404, cause: 'new password', message: 'Bad Request', questions: [] };
   }
 }
--- nextjs-app/src/app/recover-password/page.tsx
+++ nextjs-app/src/app/recover-password/page.tsx
@@ -3,7 +3,10 @@
 import Header from "@/components/Header";
 import Nav from "@/components/Nav";
 import { useState } from "react";
-import { getQuestionsByEmail } from "../../../actions/recoverPassword";
+import { getQuestionsByEmail, getSecretAnswer, recoverPassword } from "../../../actions/recoverPassword";
+import Image from "next/image";
+import check from "../../assets/icons/check.svg"
+import { useRouter } from "next/navigation";
 
 export default function RecoverPasswordPage() {
   const [email, setEmail] = useState("")
@@ -11,8 +14,12 @@ export default function RecoverPasswordPage() {
     questionId: 0,
     answer: ""
   })
-  const [newPassword, setNewPassword] = useState("")
-  const [response, setResponse] = useState({ message: "", status: 0, questions: [{id: 0, question: ""}] })
+  const [newPassword, setNewPassword] = useState({
+    password: "",
+    matchPassword: ""
+  })
+  const [response, setResponse] = useState({ status: 0, cause: "", message: "", questions: [] })
+  const [secretQuestions, setSecretQuestions] = useState([{ id: 0, question: "" }])
   const [screen, setScreen] = useState(1)
 
   const onChange = (e, state, setState) => {
@@ -24,20 +31,41 @@ export default function RecoverPasswordPage() {
     const result: any = await getQuestionsByEmail(email);
     setResponse(result)
     if (result.status === 200) {
-      console.log(result.questions)
+      setSecretQuestions(result.questions)
       setScreen(2)
     }
+  }
+
+  async function verifySecretAnswer() {
+    const result: any = await getSecretAnswer(email, questionAndAnswer);
+    setResponse(result)
+    if (result.status === 200) {
+      setScreen(3)
+    }
+  }
+
+  async function updatePassword() {
+    const result: any = await recoverPassword(email, newPassword);
+    setResponse(result)
+    if (result.status === 200) {
+      setScreen(4)
+    }
+  }
+
+  const router = useRouter();
+  function goToHomePage() {
+    router.push("/home")
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start ">
       <Header />
       <Nav>
-        <div className="h-header flex flex-col gap-12 justify-center items-center ">
-          <h1 className="text-h1 text-primary-1 font-weight-bold">Recuperar senha</h1>
-          <div className="flex flex-col w-full max-w-sm">
-            {screen === 1 ?
-              <>
+        <div className="h-header flex flex-col justify-center items-center ">
+          {screen === 1 ?
+            <>
+              <h1 className={`text-h1 mb-12 text-primary-1 font-weight-bold`}>Recuperar senha</h1>
+              <div className="flex flex-col w-full max-w-sm">
                 <p className="text-p text-neutral-2 py-1">E-mail</p>
                 <input
                   placeholder="Insira seu email"
@@ -54,39 +82,88 @@ export default function RecoverPasswordPage() {
                     {response.status === 401 && response.message}
                   </p>
                 </span>
-              </>
-              :
+                <button type="button" onClick={() => verifyEmail()} className="highlight-btn min-w-60 mt-12">Continuar</button>
+              </div>
+            </>
+            : screen === 2 ?
               <>
-                <select
-                  value={questionAndAnswer.questionId}
-                  onChange={(e) => onChange(e, questionAndAnswer, setQuestionAndAnswer)}
-                  name="questionId"
-                  id="questionId"
-                  className="h-10 mb-2 w-full focus:outline-none focus:shadow-none bg-white border border-primary-1 rounded-md pl-3 pr-10 text-neutral-2 text-p"
-                >
-                  <option value={0} disabled className="text-neutral-3 ">Escolha uma pergunta</option>
-                  {response.status === 200 && response.questions.map(item => <option key={item.id} value={item.id}>{item.question}</option>)}
-                </select>
-                <input
-                  placeholder="Resposta secreta"
-                  type="text"
-                  name="answer"
-                  value={questionAndAnswer.answer}
-                  onChange={(e) => onChange(e, questionAndAnswer, setQuestionAndAnswer)}
-                  className={`w-full p-3 border border-solid rounded text-p focus:outline-none focus:shadow-none text-neutral-2`}
-                  // ${response.error === 'answer 1' ? 'border-red-500' : 'border-primary-1'}
-                />
-                <span className="h-8 w-full flex justify-end items-center">
-                  <p className="text-red-500 text-sm">
-                    {/* {response.error === 'answer 1' && response.message} */}
-                  </p>
-                </span>
+                <h1 className={`text-h1 mb-12 text-primary-1 font-weight-bold`}>Recuperar senha</h1>
+                <div className="flex flex-col w-full max-w-sm">
+                  <select
+                    value={questionAndAnswer.questionId}
+                    onChange={(e) => onChange(e, questionAndAnswer, setQuestionAndAnswer)}
+                    name="questionId"
+                    id="questionId"
+                    className={`h-10 mb-2 w-full focus:outline-none focus:shadow-none bg-white border rounded-md pl-3 pr-10 text-neutral-2 text-p ${response.cause === 'question' ? 'border-red-500' : 'border-primary-1'}`}
+                  >
+                    <option value={0} disabled className="text-neutral-3 ">Escolha uma pergunta</option>
+                    {secretQuestions.map(item => <option key={item.id} value={item.id}>{item.question}</option>)}
+                  </select>
+                  <input
+                    placeholder="Insira sua resposta"
+                    type="text"
+                    name="answer"
+                    value={questionAndAnswer.answer}
+                    onChange={(e) => onChange(e, questionAndAnswer, setQuestionAndAnswer)}
+                    className={`w-full p-3 border border-solid rounded text-p focus:outline-none focus:shadow-none text-neutral-2  ${response.cause === 'answer' ? 'border-red-500' : 'border-primary-1'}`}
+                  />
+                  <span className="h-8 w-full flex justify-end items-center">
+                    <p className="text-red-500 text-sm">
+                      {response.status === 401 && response.message}
+                    </p>
+                  </span>
+                  <button type="button" onClick={() => verifySecretAnswer()} className="highlight-btn min-w-60 mt-12">Continuar</button>
+                </div>
               </>
-            }
-          </div>
-          <button type="button" onClick={() => verifyEmail()} className="highlight-btn min-w-60">Continuar</button>
+              : screen === 3 ?
+                <>
+                  <h1 className={`text-h1 mb-12 text-primary-1 font-weight-bold`}>Recuperar senha</h1>
+                  <div className="flex flex-col w-full max-w-sm">
+                    <p className="text-p text-neutral-2 py-1">Nova senha</p>
+                    <input
+                      placeholder="Insira sua senha"
+                      required
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={newPassword.password}
+                      onChange={(e) => onChange(e, newPassword, setNewPassword)}
+                      className={`w-full p-3 border border-solid rounded text-p text-neutral-2 ${response.cause === 'password' ? 'border-red-500' : 'border-primary-1'}`}
+                    />
+                    <span className="h-4 pt-1 w-full flex justify-end items-center">
+                      <p className="text-red-500 text-sm">
+                        {response.cause === "password" && response.message}
+                      </p>
+                    </span>
+                    <p className="text-p text-neutral-2 py-1">Confirmar senha</p>
+                    <input
+                      placeholder="Insira novamente sua senha"
+                      required
+                      type="password"
+                      name="matchPassword"
+                      id="matchPassword"
+                      value={newPassword.matchPassword}
+                      onChange={(e) => onChange(e, newPassword, setNewPassword)}
+                      className={`w-full p-3 border border-solid rounded text-p text-neutral-2 ${response.cause === 'match password' ? 'border-red-500' : 'border-primary-1'}`}
+                    />
+                    <span className="h-4 pt-1 w-full flex justify-end items-center">
+                      <p className="text-red-500 text-sm">
+                        {response.cause === "match password" && response.message}
+                      </p>
+                    </span>
+                    <button type="button" onClick={() => updatePassword()} className="highlight-btn min-w-60 mt-12">Continuar</button>
+                  </div>
+                </>
+                :
+                <>
+                  <Image alt="check" src={check} className="mb-8" />
+                  <h1 className={`text-h1 mb-4 text-primary-1 font-weight-bold`}>Senha redefinida</h1>
+                  <h4 className={`text-h4 mb-24 text-primary-1 font-weight-bold`}>Sua nova senha foi salva.</h4>
+                  <button onClick={() => goToHomePage()} className="highlight-btn min-w-60">Ir para página inicial</button>
+                </>
+          }
         </div>
       </Nav>
-    </main>
+    </main >
   )
 }
