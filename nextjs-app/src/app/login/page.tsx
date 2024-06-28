9f70761de207b95893cf3693664c1f23d1156d3e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-06-28 21:32:42 +0200

    refactor: link to recover password page

--- nextjs-app/src/app/login/page.tsx
+++ nextjs-app/src/app/login/page.tsx
@@ -6,17 +6,18 @@ import Nav from "@/components/Nav";
 import Link from "next/link";
 import { useFormState } from "react-dom";
 import { useRouter } from "next/navigation";
-import axios from "axios";
 import { login } from "../../../actions/login";
 
 export default function LoginPage() {
-  const [formState, formAction] = useFormState(login, {status: 0, message: ""});
+  const [formState, formAction] = useFormState(login, { status: 0, message: "" });
   const router = useRouter();
-  
-  console.log(formState)
 
   if (formState.status === 200) {
     router.push("/home")
+  }
+
+  function goToRecoverPasswordPage() {
+    router.push("/recover-password")
   }
 
   return (
@@ -24,7 +25,7 @@ export default function LoginPage() {
       <Header />
       <Nav>
         <div className="h-header flex flex-col gap-12 justify-center items-center">
-          <h1 className="text-h1 text-primary-1 font-weight-bold">Entrar</h1>
+          <h1 className="text-primary-1 font-weight-600">Entrar</h1>
           <form action={formAction} className="flex flex-col items-center w-80">
             <div className="flex flex-col w-full gap-3">
               <Input
@@ -47,8 +48,8 @@ export default function LoginPage() {
             </span>
             <button id="signup" type="submit" className="highlight-btn min-w-full">Entrar</button>
           </form>
-          <div className="flex flex-col items-center">
-            <p className="text-neutral-2">Esqueceu sua senha?</p>
+          <div className="flex flex-col items-center gap-2">
+            <p onClick={() => goToRecoverPasswordPage()} className="text-primary-1 cursor-pointer">Esqueceu sua senha?</p>
             <p className="text-neutral-2">Não possui uma conta? <Link href="/signup" className="text-primary-1">Criar</Link></p>
           </div>
         </div>
