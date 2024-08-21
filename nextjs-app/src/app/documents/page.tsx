74985f24023a315c9129b37f49bc7df8a9812c0d
Junior Alves <jrnalves@gmail.com>
2024-08-21 14:02:53 -0300

    style: Create the styles file with the text editor css

--- nextjs-app/src/app/documents/page.tsx
+++ nextjs-app/src/app/documents/page.tsx
@@ -2,13 +2,14 @@
 import { useQuill } from "react-quilljs";
 
 import 'quill/dist/quill.snow.css';
+import './styles.css';
 
 export default function Page() {
     const { quillRef } = useQuill({ theme: 'snow' });
 
     return (
-        <div className="w-screen h-screen bg-neutral-4">
+        <main id="container" className="w-screen h-screen bg-neutral-4">
             <div ref={quillRef}></div>
-        </div>
+        </main>
     )
 }
