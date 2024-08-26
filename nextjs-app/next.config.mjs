2ab51e6051b9f9ee612ff4b724bcde01698e7a5e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-08-26 23:53:00 +0200

    chore: svgr/webpack package

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+
+  webpack(config) {
+    // Adiciona o loader do SVGR para arquivos SVG
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
